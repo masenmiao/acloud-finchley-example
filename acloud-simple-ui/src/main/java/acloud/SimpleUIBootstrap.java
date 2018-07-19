e79a8501155912532f8d79846f50116f5fbdcd46
masenmiao <miaosen@bbktel.com>
2018-07-19 20:12:38 +0800

    update consul config

--- acloud-simple-ui/src/main/java/acloud/SimpleUIBootstrap.java
+++ acloud-simple-ui/src/main/java/acloud/SimpleUIBootstrap.java
@@ -11,7 +11,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -22,7 +21,7 @@ import org.springframework.web.client.RestTemplate;
 import feign.Request;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableHystrix
 @EnableCircuitBreaker
 @EnableFeignClients
