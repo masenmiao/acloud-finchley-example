21f7896023a94dc2aaf8cf8172e96130eda02eb6
masenmiao <miaosen@bbktel.com>
2018-07-21 18:27:44 +0800

    update consul

--- acloud-turbine/src/main/java/acloud/TurbineApplication.java
+++ acloud-turbine/src/main/java/acloud/TurbineApplication.java
@@ -3,12 +3,10 @@ package acloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableTurbine
-@EnableHystrixDashboard
 @EnableCircuitBreaker
 public class TurbineApplication {
 
